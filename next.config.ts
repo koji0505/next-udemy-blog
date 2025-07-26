73a85d2cf16ffdfe61e34e4a065fc85b595c9f52
koji0505 <ksagawa0505@gmail.com>
2025-07-27 07:04:44 +0900

    add supabase setting

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,11 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'picsum.photos',
       },
+      {
+        protocol: 'https',
+        hostname: 'ichzbuoylchzlqqzbvmt.supabase.co',
+        // pathname: '/storage/v1/object/public/udemy-next-blog-bucket/**',
+      },
     ],
   },
   experimental: {
